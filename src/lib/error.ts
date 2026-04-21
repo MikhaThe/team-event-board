7240e8ba7d8c92a0b29cbd55fab0fbfd61c4c453
derek-sal-liz <dereksalguero1021@gmail.com>
2026-04-21 15:18:50 -0400

    refactor: move RSVP errors to lib\error

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -1,3 +1,7 @@
 export type EventDetailError =
   | { name: "EventNotFound"; message: string }
   | { name: "Forbidden"; message: string }
+
+export type RSVP =
+  | { name: "RSVP Not Found"; message: string }
+  | { name: "Invalid RSVP"; message: string };
