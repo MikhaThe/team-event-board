356346d75e47a8f514d7c86201500ebccc528253
derek-sal-liz <dereksalguero1021@gmail.com>
2026-04-28 15:24:28 -0400

    feat: add SQL table for RSVP

--- prisma/migrations/20260428155856_add_event_model/migration.sql
+++ prisma/migrations/20260428155856_add_event_model/migration.sql
@@ -15,3 +15,13 @@ CREATE TABLE "Event" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
+
+CREATE TYPE user_status AS ENUM ('going', 'waitlisted', 'cancelled');
+
+CREATE TABLE "RSVP" {
+    "userId" TEXT NOT NULL,
+    "eventID" TEXT NOT NULL,
+    "status" user_status,
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAT" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+}
